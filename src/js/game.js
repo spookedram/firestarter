c70f17134176a6baed54829a7cc0ab9e6a5cea49
Zach Allen <zallen@aetn.org>
2017-01-25 11:48:43 -0600

    Added initial files for framework

---
+++ src/js/game.js
@@ -0,0 +1,10 @@
+var config = {
+    width: 640,
+    height: 480,
+    renderer: Phaser.AUTO,
+    parent: "game-container",
+    antialias: true,
+    multiTexture: true
+};
+
+var game = new Phaser.Game(config);
